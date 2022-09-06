679ea5ee58bd4043bb5eb8e969122473fe02202d
Code A Program <75136330+Sridhar-C-25@users.noreply.github.com>
2022-09-06 09:20:14 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Stepper from "./components/Stepper/Stepper";
+const App = () => {
+  return (
+    <div className="bg-gray-900 flex flex-col gap-10 h-screen items-center justify-center">
+      <Stepper />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Stepper/Stepper.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./stepper.css";
+import { TiTick } from "react-icons/ti";
+const Stepper = () => {
+  const steps = ["Customer Info", "Shipping Info", "Payment", "Step 4"];
+  const [currentStep, setCurrentStep] = useState(1);
+  const [complete, setComplete] = useState(false);
+  return (
+    <>
+      <div className="flex justify-between">
+        {steps?.map((step, i) => (
+          <div
+            key={i}
+            className={`step-item ${currentStep === i + 1 && "active"} ${
+              (i + 1 < currentStep || complete) && "complete"
+            } `}
+          >
+            <div className="step">
+              {i + 1 < currentStep || complete ? <TiTick size={24} /> : i + 1}
+            </div>
+            <p className="text-gray-500">{step}</p>
+          </div>
+        ))}
+      </div>
+      {!complete && (
+        <button
+          className="btn"
+          onClick={() => {
+            currentStep === steps.length
+              ? setComplete(true)
+              : setCurrentStep((prev) => prev + 1);
+          }}
+        >
+          {currentStep === steps.length ? "Finish" : "Next"}
+        </button>
+      )}
+    </>
+  );
+};
+
+export default Stepper;
