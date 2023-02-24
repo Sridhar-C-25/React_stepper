c31fcad4b02429d8077a32021ca116cb085c97e0
Fatih Çınar <42092052+thefatihcinar@users.noreply.github.com>
2023-02-24 23:41:40 +0300

    created gitignore

--- src/components/Stepper/Stepper.jsx
+++ src/components/Stepper/Stepper.jsx
@@ -1,12 +1,36 @@
-import React, { useState } from "react";
-import "./stepper.css";
+import './stepper.css';
+import { useState } from 'react';
 import { TiTick } from "react-icons/ti";
-const Stepper = () => {
-  return (
-    <div>
 
-    </div>
-  );
+const Stepper = () => {
+
+  const steps = ["Customer Info", "Shipping Info", "Payment"];
+  const [currentStep, setCurrentStep] = useState(1);
+  const [complete, setComplete] = useState(false);
+
+  return (
+    <>
+      <div className="flex justify-between">
+          {
+            steps?.map(( step, index ) => (
+              <div key = {index} className={`step-item ${currentStep === index + 1 && "active"} ${(complete || index + 1 < currentStep) && "complete"}`}>
+                <div className="step">
+                  { (index + 1 < currentStep || complete) ? <TiTick size={24}/> : index + 1}
+                </div>
+                <p className="text-gray-500">{step}</p>
+              </div>
+            ))
+          }   
+      </div>
+      <button className={`btn ${complete && "complete"}`} onClick={() => {
+        if(currentStep === steps.length ) setComplete(true);
+        else setCurrentStep( previous => previous + 1);
+      }}>{
+        complete ? "Finish" : "Next"
+      }</button>
+    </>
+    );
+
 };
 
 export default Stepper;
